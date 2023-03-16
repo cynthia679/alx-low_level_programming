08da983880195a1b0d544bd4427d98a9b4810e3f
cynthia679 <cynthiachebet5@gmail.com>
2023-03-16 03:31:12 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
  * main - Programming is like building a multilingual puzzle.
  *Return: Always 0.
  */
-int main(void){   
+int main(void)
+{   
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }	
